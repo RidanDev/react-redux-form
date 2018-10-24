94ff452c1a68849fdfac18ee36dd5a47cfd45cf1
Villalba <gianluca.nadir.villalba@capgemini.com>
2018-10-24 17:50:30 +0200

    SyncValidationForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import SimpleForm from './SimpleForm'
+import SyncValidationForm from './SyincValidationForm'
 import showResults from './showResults'
 
 class App extends Component {
   render() {
     return (
-        <SimpleForm onSubmit={showResults} />
+        //<SimpleForm onSubmit={showResults} />
+        <SyncValidationForm onSubmit={showResults} />
     );
   }
 }
